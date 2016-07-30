3623dff576d99f7a23dca10abe6d1339c47f04ca
Dominic Tarr <dominic.tarr@gmail.com>
2016-07-30 12:50:21 +1200

    make sure everything is a buffer

--- index.js
+++ index.js
@@ -8,21 +8,31 @@ exports.crypto_hash_sha256 = function (msg) {
   return new Sha256().update(msg).digest()
 }
 
+function fix_keys(keys) {
+  return {
+    publicKey: new Buffer(keys.publicKey),
+    secretKey: new Buffer(keys.secretKey),
+  }
+}
+
 exports.crypto_sign_seed_keypair = function (seed) {
-  return tweetnacl.sign.keyPair.fromSeed(seed)
+  return fix_keys(tweetnacl.sign.keyPair.fromSeed(seed))
 }
 
 exports.crypto_sign_keypair = function () {
-  return tweetnacl.sign.keyPair()
+  return fix_keys(tweetnacl.sign.keyPair())
 }
 
 exports.crypto_sign_detached = function (msg, skey) {
-  return tweetnacl.sign.detached(msg, skey)
+  return new Buffer(tweetnacl.sign.detached(msg, skey))
 }
 
-exports.crypto_sign = tweetnacl.sign
-
-exports.crypto_sign_open = tweetnacl.sign.open
+exports.crypto_sign = function (msg, sk) {
+  return new Buffer(tweetnacl.sign(msg, sk))
+}
+exports.crypto_sign_open = function (ctxt, pk) {
+  return new Buffer(tweetnacl.sign.open(ctxt, pk))
+}
 
 exports.crypto_sign_verify_detached = function (sig, msg, pkey) {
   return tweetnacl.sign.detached.verify(msg, sig, pkey)
@@ -33,23 +43,38 @@ exports.crypto_box_keypair = function () {
 }
 
 
-exports.crypto_hash = tweetnacl.hash
+exports.crypto_hash = function (msg) {
+  return new Buffer(tweetnacl.hash(msg))
+}
 
-exports.crypto_secretbox_easy = tweetnacl.secretbox
+exports.crypto_secretbox_easy = function (msg, key, nonce) {
+  return new Buffer(tweetnacl.secretbox(msg, key, nonce))
+}
+
 exports.crypto_secretbox_open_easy = function (ctxt, nonce, key) {
-  return tweetnacl.secretbox.open(ctxt, nonce, key) || null
+  var r = tweetnacl.secretbox.open(ctxt, nonce, key)
+  return r ? new Buffer(r) : null
 }
 
-exports.crypto_sign_ed25519_pk_to_curve25519 = ed2curve.convertPublicKey
-exports.crypto_sign_ed25519_sk_to_curve25519 = ed2curve.convertSecretKey
+exports.crypto_sign_ed25519_pk_to_curve25519 = function (pk) {
+  return new Buffer(ed2curve.convertPublicKey(pk))
+}
+exports.crypto_sign_ed25519_sk_to_curve25519 = function (sk) {
+  return new Buffer(ed2curve.convertSecretKey(sk))
+}
 
+exports.crypto_box_easy = function (msg, nonce, pkey, skey) {
+  return new Buffer(tweetnacl.box(msg, nonce, pkey, skey))
+}
 
-exports.crypto_box_easy = tweetnacl.box
 exports.crypto_box_open_easy = function (ctxt, nonce, pkey, skey) {
-  return tweetnacl.box.open(ctxt, nonce, pkey, skey) || null
+  var r = tweetnacl.box.open(ctxt, nonce, pkey, skey)
+  return r ? new Buffer(r) : null
 }
 
-exports.crypto_scalarmult = tweetnacl.scalarMult
+exports.crypto_scalarmult = function (pk, sk) {
+  return new Buffer(tweetnacl.scalarMult(pk, sk))
+}
 
 //exports.crypto_auth = tweetnacl.auth
 //exports.crypto_auth_verify = tweetnacl.auth.verify
@@ -67,4 +92,11 @@ exports.crypto_auth_verify = function (mac, msg, key) {
   }
   return +!d
 }
+
+exports.randombytes = function (buf) {
+  var b = new Buffer(tweetnacl.randomBytes(buf.length))
+  b.copy(buf)
+  return null
+}
+
 
